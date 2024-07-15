d1eed35d21b1ee4a0dc3d8c8665fb5a21618952e
soojin-p <72116811+soojin-p@users.noreply.github.com>
2024-07-15 21:38:43 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 111.06 ms, Memory: 148 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+
+        long sum1 = 0;
+        for (int num : queue1) sum1 += num;
+        long totalSum = sum1;
+        for (int num : queue2) totalSum += num;
+
+        if (totalSum % 2 != 0) return -1;
+
+        long target = totalSum / 2;
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        for (int num : queue1) q1.offer(num);
+        for (int num : queue2) q2.offer(num);
+
+        // 합이 목표합과 같아질 때가지 반복
+        int maxOperation = (queue1.length + queue2.length) * 2; 
+        while (sum1 != target) {
+            if (answer > maxOperation) return -1;
+            if(sum1 == target){
+                return answer;
+            }
+            if (sum1 > target) {
+                int num = q1.poll();
+                q2.offer(num);
+                sum1 -= num;
+            } else {
+                int num = q2.poll();
+                q1.offer(num);
+                sum1 += num;
+            }
+            answer++; 
+        }
+        return answer;
+    }
+}
