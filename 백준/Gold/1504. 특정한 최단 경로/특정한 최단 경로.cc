56bb79c52b3735939894d9dada9a7878232aa4e7
soojin-p <72116811+soojin-p@users.noreply.github.com>
2025-04-01 20:27:30 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 128 ms, Memory: 5776 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -10,56 +10,59 @@ vector<pair<int, int>> graph[8001];
 
 const int INF=1e9;
 
-vector<int> dijstra(int start, int n){
-    vector<int> dist(n + 1, INF);
-    priority_queue<pair<int,int>,vector<pair<int, int>>, greater<>> pq;
-    dist[start] =0;
+vector<int> dijkstra(int start, int n){
+    vector<int>dist(n+1, INF);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>,greater<>> pq;
+    dist[start]=0;
     pq.push({0,start});
+    
+   while (!pq.empty())
+   {
+     auto[cost, now] = pq.top();
+     pq.pop();
 
-    while(!pq.empty()){
-        auto[cur_cost,cur_node] = pq.top();
-        pq.pop();
+     if(dist[now]<cost) continue;
 
-        if(dist[cur_node]<cur_cost) continue;
+     for(auto[next, w]: graph[now]){
+        int next_cost = dist[now]+w;
         
-        for(auto[next_node, weight]: graph[cur_node]){
-            int next_cost = cur_cost+weight;
-            if(next_cost<dist[next_node]){
-                dist[next_node] = next_cost;
-                pq.push({next_cost,next_node});
-            }
+        if(next_cost <dist[next]){
+            dist[next]= next_cost;
+            pq.push({next_cost,next});
         }
-    }
-    return dist;
+     }
 
+    /* code */
+   }
+   return dist;
 }
 
 int main(){
-
     int N, E;
-    cin >>N>>E;
+    cin >>N>> E;
 
-    for(int i=0; i<E; i++){
-        int a,b,c;
-        cin >> a>>b>>c;
+    for(int i=0; i< E; i++){
+        int a, b, c;
+        cin >>a>>b>>c;
         graph[a].emplace_back(b,c);
         graph[b].emplace_back(a,c);
-
     }
     int v1, v2;
-    cin >> v1 >>v2;
+    cin >>v1>>v2;
 
-    vector<int> dist_start = dijstra(1,N);
-    vector<int> dist_v1 = dijstra(v1,N);
-    vector<int> dist_v2 = dijstra(v2,N);
+    vector<int> start_dist = dijkstra(1,N);
+    vector<int> v1_dist = dijkstra(v1,N);
+    vector<int> v2_dist = dijkstra(v2,N);
 
-    int path1 = dist_start[v1] + dist_v1[v2]+dist_v2[N];
-    int path2 = dist_start[v2] + dist_v2[v1] + dist_v1[N];
+    int path1 = start_dist[v1]+ v1_dist[v2] + v2_dist[N];
+    int path2 = start_dist[v2] + v2_dist[v1] + v1_dist[N];
 
     int result = min(path1,path2);
 
-    if(result >= INF || dist_v1[v2]>=INF) cout<< -1;
+    if(result>=INF || v1_dist[v2]>=INF) cout<< -1;
     else cout<<result;
 
     return 0;
+
 }
