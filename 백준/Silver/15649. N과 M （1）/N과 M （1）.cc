f15cacdf1cddb075e0880902a16c9557d61db349
soojin-p <72116811+soojin-p@users.noreply.github.com>
2025-04-10 13:32:41 +0900

    [Silver III] Title: N과 M (1), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,38 +1,42 @@
-#include <iostream>
-#include <sstream>
+#include<iostream>
+#include <vector>
+
+
 using namespace std;
 
-int N, M;
+
+int N,M;
 bool visited[9];
 int arr[9];
-ostringstream result;
-
-void dfs(int depth) {
-    if (depth == M) {
-        for (int i = 0; i < M; i++) {
-            result << arr[i] << ' ';
-        }
-        result << '\n';
-        return;
+void funcprint(){
+    for(int i=0; i<M; i++){
+        cout<<arr[i]<<" ";
     }
-
-    for (int i = 1; i <= N; i++) {
-        if (!visited[i]) {
-            visited[i] = true;
-            arr[depth] = i;
-            dfs(depth + 1);
-            visited[i] = false;
-        }
-    }
+    cout<<"\n";
 }
+void dfs(int depth){
+ 
+    if(depth==M){
+        // 출력
+        funcprint();
+        return; 
+    }
+    for(int i=1; i<=N; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            arr[depth]=i;
+            dfs(depth+1);
+            visited[i]=false;
+        }
+    }
 
-int main() {
+}
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> N >> M;
-    dfs(0);
-
-    cout << result.str();  // 한 번에 출력
+    cin>>N>>M;
+   
+   dfs(0);
     return 0;
 }
