0e6c57856622a36dcdcb3c1b4c3ea4b7f80dc537
soojin-p <72116811+soojin-p@users.noreply.github.com>
2025-05-17 15:18:02 +0900

    [level 2] Title: 타겟 넘버, Time: 6.06 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,20 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <string>
+#include <vector>
 
-int ans =0;
-void dfs(vector<int> &numbers, int target, int idx, int sum){
-   if(idx == numbers.size()){
-       if(sum==target) {
-           ans++ ;}
-        return;
-   }
-    dfs(numbers, target,idx+1,sum+numbers[idx]);// 현재숫자
-    dfs(numbers, target,idx+1,sum-numbers[idx]);
-}
+using namespace std;
+int ans=0;
+void func(vector<int> &numbers, int target, int sum, int idx){
+    
+    if(idx== numbers.size()){
+        if(sum==target){
+            ans++;
+        }
+            return;
+        }
+     func(numbers, target,sum + numbers[idx], idx+1);
+    func(numbers, target,sum - numbers[idx], idx+1);
+    }
+   
+    
+
 int solution(vector<int> numbers, int target) {
     int answer = 0;
     
-    dfs(numbers,target,0,0);
+   func(numbers, target, 0,0);
     answer = ans;
     return answer;
 }
