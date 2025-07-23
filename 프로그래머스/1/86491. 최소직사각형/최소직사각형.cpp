c332c65fca37f7ff65579bef964139ccceb9bb4d
soojin-p <72116811+soojin-p@users.noreply.github.com>
2025-07-23 20:12:35 +0900

    [level 1] Title: 최소직사각형, Time: 0.92 ms, Memory: 4.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,34 +1,16 @@
-#include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
-    int answer = 0;
-    vector<int> orderd_r();
-    vector<int> orderd_c();
-    sort(sizes.begin(), sizes.end());
-    for(int i=0; i< sizes.size(); i++){
-        if(sizes[i][0]<sizes[i][1]){
-            int tmp = sizes[i][0];
-            sizes[i][0] = sizes[i][1];
-            sizes[i][1] = tmp;
-        }
+    int max_w = 0, max_h = 0;
+
+    for (auto& card : sizes) {
+        int w = max(card[0], card[1]);
+        int h = min(card[0], card[1]);
+        max_w = max(max_w, w);
+        max_h = max(max_h, h);
     }
-    int max_r=0;
-    for(int i=0; i<sizes.size(); i++){
-        if(sizes[i][0]>max_r){
-            max_r = sizes[i][0];
-        }
-    }
-    int max_c=0;
-    for(int i=0; i<sizes.size(); i++){
-        if(sizes[i][1]>max_c){
-            max_c = sizes[i][1];
-        }
-    }
-    answer = max_r*max_c;
-   
-    return answer;
+
+    return max_w * max_h;
 }
