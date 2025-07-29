7e96c046996cf78482a8d1aef00ed42515e6349e
soojin-p <72116811+soojin-p@users.noreply.github.com>
2025-07-29 21:39:38 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,24 +1,39 @@
 #include <string>
 #include <vector>
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-// a b c d e f g h i j k l m
-// n o p q r s t u v w x y z
-// 21-9 = 12
+/*
+a b c d e
+f g h i j k l m n 
+o p q r s t u
+v w x y z
 
+
+*/
 int solution(string name) {
     int answer = 0;
-    int len = name.length();
-    for(char c: name){
-        answer+= min(c-'A', 'Z'-c+1);
+    int n = name.length();
+    for(int i=0; i<n; i++){
+        if(name[i]<='N'){
+            answer+=name[i]-'A';
+        }else{
+            answer+='Z'-name[i]+1;
+        }
     }
-    int move = len-1;
-    for(int i=0; i<len; i++){
+    cout<<answer;
+    
+    int move=n-1;
+    for(int i=0;i<n; i++){
         int next = i+1;
-        // len- next : next인덱스 부터 끝까지 처리해야하는 문자 수
-        while (next<len && name[next]=='A') next++;
-        move = min(move, i + len - next + min(i, len-next));
+        while(next<n && name[next]=='A'){
+                next++;
+            }
+        // B C A B B B
+        //왼쪽으로 갔다가 돌아오는 경우
+        move = min(move,i+i+n-next);
+        // 끝까지 가는 꼉우
+        move = min(move,(n-next)*2+i);
     }
-    answer +=move;
+    answer+=move;
     return answer;
 }
