e7b7c3548dc72139357f2f4c340afe941f3694a1
soojin-p <72116811+soojin-p@users.noreply.github.com>
2025-10-08 15:58:27 +0900

    Update 부분수열의 합.cc

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
@@ -2,10 +2,10 @@
 
 using namespace std;
 /*
-문제이해
 
 풀이
-
+- 백트래킹으로 모든 부분합을 탐색하며 가능한 합을 기록
+- 0부터 총합까지 중 방문되지 않은 첫 번째 자연수가 정답
 */
 
 vector<int> nums;
@@ -38,3 +38,4 @@ int main() {
     cout<<ans;
     return 0;
 }
+
